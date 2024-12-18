f6189e2a9af82f5f27837468567145fd4e572668
Gabriel Henrique <gabidelnovaes@gmail.com>
2024-12-18 10:22:12 -0300

    Importing sounds, JS Logic and HTML MIDI Design

---
+++ main.js
@@ -0,0 +1,17 @@
+function playSound(id){
+    document.querySelector(id).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for(let i=0; i < listaDeTeclas.length;i++){
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        playSound(idAudio);
+    };
+
+}
